030e5f8820d37c24b8b1762fb51ad86ce6470c8e
Matt-Blanco <mattmblanco@gmail.com>
2025-12-05 16:10:11 -0500

    Create grid rendering logic

--- sketch.js
+++ sketch.js
@@ -1,7 +1,69 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(220);
+  background("white");
+  // drawPointGrid(50, 50, 50);
+  let grid = new Grid(50, 50, 60)
+  grid.draw();
 }
+
+function keyPressed() {
+  console.log(key);
+  if (key.toLocaleUpperCase() === 'm') {
+    
+  }
+}
+
+class Grid {
+  size;
+  startX;
+  startY;
+
+  constructor(startX, startY, size) {
+    this.startX = startX;
+    this.startY = startY;
+    this.size = size;
+
+    let count = 0;
+    const inc = this.size / 3;
+    for (let rows = 0; rows < 3; rows++) {
+      for (let col = 0; col < 3; col++) {
+        let x = this.startX + ((rows + 1) * inc)
+        let y = this.startY + ((col + 1) * inc)
+
+        const p = new Point(x, y, `p${count}`)
+        this.points.push(p);
+        count++
+      }
+    }
+  }
+
+  points = [];
+
+  draw() {
+    noStroke();
+    fill("black");
+
+    this.points.forEach(p => p.draw())
+  }
+}
+
+const radius = 2;
+class Point {
+  x;
+  y;
+  label;
+
+  constructor(x, y, label) {
+    this.x = x;
+    this.y = y;
+    this.label = label;
+  }
+
+  draw() {
+    circle(this.x, this.y, radius)
+  }
+}
+
