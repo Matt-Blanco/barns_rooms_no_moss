7f7c8cf8b07ec5aca72cc74b6fd455ac5d91c5dc
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-01-05 19:13:01 -0600

    working two degrees of rotation.

--- sketches/sketch-1.js
+++ sketches/sketch-1.js
@@ -25,46 +25,58 @@ const c_size = 1000;
 let base_x = margin;
 let base_y = margin;
 
+//rotation bookkeeping
+let rotationStep = 0;
+
 function preload() {
   font = loadFont("../assets/CrimsonText-Regular.ttf");
 }
 
 function setup() {
-  createCanvas(c_size, c_size, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 
-  base_x = width/2; 
-  base_y = height/2;
+  base_x = width / 2;
+  base_y = height / 2;
 
   //set defaults:
   textFont(font);
-  textSize(16);
+  textSize(t_size);
   noStroke();
   fill(0);
 }
 
 function draw() {
   background(255);
-  //orbit control: 
-    orbitControl();
+
+  //orbit control:
+  orbitControl();
 
   //global translate for all drawing operations.
   translate(-width / 2, -height / 2, 0);
 
+  //draw committed words
   for (let word of words) {
     word.display();
   }
 
   //also, when being typed, show what is being typed.
-  base_x = words.length < 1 ? width/2 : words[words.length - 1].x + words[words.length - 1].tw + t_size / 2;
+  base_x = words.length < 1 ? width / 2 : words[words.length - 1].x + words[words.length - 1].tw + t_size / 2;
 
   fill(190);
   text(str, base_x, base_y);
 
-  indicator({
-    x: base_x + textWidth(str),
-    y: base_y,
-  });
+  //indicator follows current rotation axis
+  push();
+  translate(base_x + textWidth(str), base_y, 0);
 
+  if (rotationStep % 2 === 0) {
+    rotateX(HALF_PI);
+  } else {
+    rotateY(HALF_PI);
+  }
+
+  indicator({ x: 0, y: 0 });
+  pop();
 }
 
 //function to draw indicator when typing.
@@ -89,13 +101,19 @@ function keyPressed() {
   if (key === " ") {
     //you have to push a new object.
     if (str.length > 0) {
-      words.push(new Word(str, base_x, base_y));
+      //alternate rotation axis every word
+      let axis = rotationStep % 2 === 0 ? "x" : "y";
+      let angle = HALF_PI; //90 degrees
+
+      words.push(new Word(str, base_x, base_y, 0, axis, angle));
 
       //advance x position after committing word
       base_x += textWidth(str) + t_size / 2;
 
       //reset accumulator string.
       str = "";
+
+      rotationStep++;
     }
   } else if (key.length === 1) {
     //accumulate it.
@@ -104,10 +122,14 @@ function keyPressed() {
 }
 
 class Word {
-  constructor(str, x, y) {
+  constructor(str, x, y, z = 0, axis = "x", angle = 0) {
     this.str = str;
     this.x = x;
     this.y = y;
+    this.z = z;
+
+    this.axis = axis;
+    this.angle = angle;
 
     this.tw = textWidth(this.str); // with this i can calculate the width of the text box to affect the other text box positions.
   }
@@ -117,8 +139,15 @@ class Word {
 
     push();
     //translation necessary for z-dimension.
-    translate(0, 0, 0);
-    text(this.str, this.x, this.y);
+    translate(this.x, this.y, this.z);
+
+    if (this.axis === "x") {
+      rotateX(this.angle);
+    } else if (this.axis === "y") {
+      rotateY(this.angle);
+    }
+
+    text(this.str, 0, 0);
     pop();
   }
 }
